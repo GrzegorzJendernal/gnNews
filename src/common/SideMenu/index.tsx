2398c5498225a69ef882dabc21bdd5df12562702
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-23 21:03:50 +0100

    added Side Menu

---
+++ src/common/SideMenu/index.tsx
@@ -0,0 +1,21 @@
+const SideMenu = () => {
+	return (
+		<div>
+			<ul>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+				<li>country</li>
+			</ul>
+		</div>
+	);
+};
+
+export default SideMenu;
