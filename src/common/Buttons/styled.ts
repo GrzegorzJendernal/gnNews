a9cf70a688233c65a8094c64fd114d839bfb5d3e
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-23 20:42:16 +0100

    styled Buttons component

---
+++ src/common/Buttons/styled.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.span`
+	display: flex;
+	gap: 50px;
+`;
+
+export const Button = styled.button`
+	height: 40px;
+	background-color: #000000;
+	color: white;
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: 600;
+  padding: 8px 24px;
+  border: 1px solid #FFFFFF;
+  border-radius: 24px;
+	cursor: pointer;
+	text-transform: uppercase;
+	
+	&:hover {
+		background-color: #4d4d4d;
+	}
+`;
