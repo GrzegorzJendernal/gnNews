7771cab6308a47d33269adc5c306778a0c9be950
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-23 20:42:06 +0100

    added Buttons component

---
+++ src/common/Buttons/index.tsx
@@ -0,0 +1,17 @@
+import { Button, Container } from "./styled";
+
+const Buttons = () => {
+
+	return (
+		<Container>
+			<Button>
+				change view to list
+			</Button>
+			<Button>
+				show more info
+			</Button>
+		</Container>
+	);
+};
+
+export default Buttons;
