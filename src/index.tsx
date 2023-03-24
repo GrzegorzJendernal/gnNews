bbea5ca8ad2d3bbcbfc7c0ceb2821709eb984d73
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-24 21:23:43 +0100

    used TanStack Query

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './core/App'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools/>
+			<App/>
+		</QueryClientProvider>
+	</React.StrictMode>,
 )
