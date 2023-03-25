ae089531aae669a13230822f95b13d95904a63a7
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-25 22:52:06 +0100

    declared types

---
+++ src/common/types/news.d.ts
@@ -0,0 +1,13 @@
+export type News = {
+	source: {
+		id: null | string;
+		name: string;
+	}
+	author: string;
+	title: string;
+	description: string;
+	url: string;
+	urlToImage: string;
+	publishedAt: string;
+	content: string;
+}
