5146d4d409597b672dcf5d592a522c9fa07ec485
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-27 20:35:24 +0200

    removed setCountry action and selectCountry selector

--- src/features/news/newsSlice.ts
+++ src/features/news/newsSlice.ts
@@ -5,23 +5,18 @@ const newsSlice = createSlice({
 	name: "news",
 	initialState: {
 		listView: true,
-		country: "pl",
 	},
 	reducers: {
 		changeView: state => {
 			state.listView = !state.listView;
 		},
-		setCountry: (state, { payload: short }) => {
-			state.country = short;
-		},
 	}
 });
 
-export const {changeView, setCountry} = newsSlice.actions;
+export const {changeView} = newsSlice.actions;
 
 const selectNewsState = (state: RootState) => state.news;
 
 export const selectView = (state: RootState) => selectNewsState(state).listView;
-export const selectCountry = (state: RootState) => selectNewsState(state).country;
 
 export default newsSlice.reducer;
