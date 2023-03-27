cfe7eae44ab449e98af3f005fd4bbf29c31bddc8
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-27 20:34:36 +0200

    created hook useSelectedCountry

---
+++ src/common/data/useSelctedCountry.ts
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import { countries } from "./countries";
+
+export const useSelectedCountry = () => {
+	const {country} = useParams();
+
+	const selectedCountry = countries
+		.find(
+			(countryURL) => countryURL.url === country
+		);
+	if (!selectedCountry) return { country: "Poland", short: "pl", url: "poland" };
+	return selectedCountry;
+};
