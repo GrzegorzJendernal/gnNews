10526418c8d8933a2142709f1f37bc3d145a93e7
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-28 22:13:03 +0200

    moved popup action and state to slice

--- src/features/news/newsSlice.ts
+++ src/features/news/newsSlice.ts
@@ -1,22 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../core/App/store";
+
+interface PopupState {
+	[key: string]: boolean;
+}
+
+interface NewsState {
+	listView: boolean;
+	popup: PopupState;
+}
+
+const initialState: NewsState = {
+	listView: true,
+	popup: {},
+};
 
 const newsSlice = createSlice({
 	name: "news",
-	initialState: {
-		listView: true,
-	},
+	initialState,
 	reducers: {
 		changeView: state => {
 			state.listView = !state.listView;
 		},
+		showPopup: (state, action: PayloadAction<string>) => {
+			const title = action.payload;
+			state.popup[title] = !state.popup[title];
+		},
 	}
 });
 
-export const {changeView} = newsSlice.actions;
+export const {changeView, showPopup} = newsSlice.actions;
 
 const selectNewsState = (state: RootState) => state.news;
 
 export const selectView = (state: RootState) => selectNewsState(state).listView;
+export const selectPopup = (state: RootState) => selectNewsState(state).popup;
 
 export default newsSlice.reducer;
