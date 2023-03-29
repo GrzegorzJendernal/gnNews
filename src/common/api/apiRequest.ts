91872d8ff23038fa91377bcee23d99f91cf3f67b
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-29 21:50:55 +0200

    added new params to request

--- src/common/api/apiRequest.ts
+++ src/common/api/apiRequest.ts
@@ -1,13 +1,16 @@
 import axios from "axios";
+import { apiKey } from "./apiKey";
 
 export const getNews = async (country: string) => {
 	const params = new URLSearchParams({
 		country: country,
+		apiKey: apiKey,
+		pageSize: "50",
 		}
 	);
 
 	const response = await axios.get(
-		`https://newsapi.org/v2/top-headlines?${params}&apiKey=717b16ebba75457990be9eaecf0cd28b`
+		`https://newsapi.org/v2/top-headlines?${params}`
 	);
 	return await response.data;
 };
