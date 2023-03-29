fcec6967b1d4ac5830cfbe714c94d978747a4350
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-29 21:51:20 +0200

    added number of articles

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -1,9 +1,15 @@
 import Clock from "./Clock";
-import { Wrapper } from "./styled";
+import { Text, Wrapper } from "./styled";
+import { useQuery } from "@tanstack/react-query";
+import { useSelectedCountry } from "../data/useSelctedCountry";
+import { getNews } from "../api/apiRequest";
 
 const Footer = () => {
+	const {short} = useSelectedCountry();
+	const {data} = useQuery(["news", {country: short}], () => getNews(short));
 	return (
 		<Wrapper>
+				{data && <Text>Articles on site: {data.totalResults}</Text>}
 			<Clock/>
 		</Wrapper>
 	);
