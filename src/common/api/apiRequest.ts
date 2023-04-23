6c14346134eff7b82a700fa405993cb96c6bd3af
GrzegorzJendernal <g.jendernal@gmail.com>
2023-04-23 20:36:36 +0200

    actualised apiRequest

--- src/common/api/apiRequest.ts
+++ src/common/api/apiRequest.ts
@@ -1,17 +1,15 @@
 import axios from "axios";
 import { apiKey } from "./apiKey";
 
-export const getNews = async (country: string) => {
-	// const params = new URLSearchParams({
+export const getNews = async () => {
+	const params = new URLSearchParams({
 	// 		country: country,
-	// 		apiKey: apiKey,
-	// 		pageSize: "50",
-	// 	}
-	// );
+			apikey: apiKey,
+		}
+	);
 
 	const response = await axios.get(
-		// `https://newsapi.org/v2/top-headlines?${params}`
-		// `https://gnews.io/api/v4/top-headlines?category=general&apikey=14010cbd996dccd43c90e07480634c41`
+		`https://gnews.io/api/v4/top-headlines?category=general&${params}`
 	);
 	return await response.data;
 };
